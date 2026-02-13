49b1756b4921d31888ea34e99b2e4811b6d3df56
nikoloc <dqrk0@proton.me>
2026-02-13 14:18:20 +0100

    minor additions

--- time_util.h
+++ time_util.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 #include <time.h>
 
-static inline uint32_t
+static inline uint64_t
 time_from_timespec_ms(struct timespec *ts) {
     return ts->tv_sec * 1000 + ts->tv_nsec / 1000000;
 }
@@ -14,7 +14,7 @@ time_from_timespec_ns(struct timespec *ts) {
     return (uint64_t)ts->tv_sec * 1000000000 + ts->tv_nsec;
 }
 
-static inline uint32_t
+static inline uint64_t
 time_now_ms(void) {
     struct timespec ts;
     clock_gettime(CLOCK_MONOTONIC, &ts);
@@ -22,12 +22,20 @@ time_now_ms(void) {
     return time_from_timespec_ms(&ts);
 }
 
-static inline double
+static inline uint64_t
+time_now_ns(void) {
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+
+    return time_from_timespec_ns(&ts);
+}
+
+static inline uint64_t
 time_delta_ns(struct timespec *a, struct timespec *b) {
     return (double)(time_from_timespec_ns(a) - time_from_timespec_ns(b));
 }
 
-static inline double
+static inline uint64_t
 time_delta_ms(struct timespec *a, struct timespec *b) {
     return time_delta_ns(a, b) / 1000000.0F;
 }
